4f1322c53128ebde4743af2e87251183376bd788
Bren Dempsey <x00132084@gmail.com>
2016-10-12 19:05:57 +0100

    Added java and class files

---
+++ BrensWorld.java
@@ -0,0 +1,7 @@
+public class BrensWorld{
+	public static void main(String[] args){
+		
+			System.out.println("Hello Bren!! Is this your world?");
+			System.out.println("Well...it bloody well might be!!!");
+	}
+}
